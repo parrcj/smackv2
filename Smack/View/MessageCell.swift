13903d883c402f45b0ac405ec79dd1381d2d7059
Chris Parr <parr.cj@gmail.com>
2019-02-11 16:54:52 +0000

    Lesson 26

---
+++ Smack/View/MessageCell.swift
@@ -0,0 +1,34 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by Christopher Parr on 11/02/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    //Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    func configureCell(message: Message){
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+    
+    
+}
